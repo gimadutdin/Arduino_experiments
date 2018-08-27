f2a2edd7b19a03f495ebd26b9243044cd5212639
gimadutdin <33692565+gimadutdin@users.noreply.github.com>
2018-08-27 22:33:30 +0300

    Add files via upload

---
+++ button_and_two_leds.ino
@@ -0,0 +1,31 @@
+//this sketch processes button pressing, also fixes contact bounce
+byte button = 2, yellow = 11, blue = 12, yellow_flag = 0, blue_flag = 1, press_flag = 0;
+unsigned long last_press = 0;//if you write byte or int it will not work good due to overflow
+
+void setup() {
+  pinMode(button, INPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(blue, OUTPUT);
+  write_leds();
+}
+
+void swap_leds()
+{
+  yellow_flag = !yellow_flag;
+  blue_flag = !blue_flag;
+}
+
+void write_leds()
+{
+  digitalWrite(yellow, yellow_flag);
+  digitalWrite(blue, blue_flag);
+}
+
+void loop() {
+  if (digitalRead(button) == HIGH && millis() - last_press > 200)
+  {
+    swap_leds();
+    write_leds();
+    last_press = millis();
+  }
+}
